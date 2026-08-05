2852d1797f1c12350e34ed0e8e4eba0ede884cf0
dejuks <dejuks@gmail.com>
2026-08-05 14:45:11 +0300

    ffl

--- routes/api.php
+++ routes/api.php
@@ -4,6 +4,7 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\Chatbot\CustomerChatbotController;
 use App\Http\Controllers\Api\Chatbot\ChatbotCategoryController;
 use App\Http\Controllers\Api\Chatbot\ChatbotTrainingQuestionController;
+use App\Http\Controllers\Api\FeedbackController;
 
 Route::middleware('auth:sanctum')->post('/chatbot/message', [CustomerChatbotController::class, 'message']);
 
@@ -26,3 +27,17 @@ require base_path('routes/user.php');
 require base_path('routes/service.php');
 require base_path('routes/window.php');
 require base_path('routes/application.php');
+
+
+Route::post('feedback', [FeedbackController::class, 'store']);
+
+Route::middleware('auth:sanctum')->group(function () {
+    Route::get('feedback', [FeedbackController::class, 'index']);
+
+    Route::get('feedback/windows', [FeedbackController::class, 'windows']);
+
+    Route::get('feedback/{feedback}', [FeedbackController::class, 'show']);
+    Route::put('feedback/{feedback}', [FeedbackController::class, 'update']);
+    Route::patch('feedback/{feedback}', [FeedbackController::class, 'update']);
+    Route::delete('feedback/{feedback}', [FeedbackController::class, 'destroy']);
+});
